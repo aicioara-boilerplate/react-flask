92d29781f0b83ec37956a0b16e68257c64e807f1
Andrei Cioara <andrei@cioara.me>
2020-05-02 13:46:03 -0700

    Minor upgrades

--- src/backend/api/mixins/timestamp_mixin.py
+++ src/backend/api/mixins/timestamp_mixin.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, event
+import sqlalchemy as sql
 from sqlalchemy.ext.declarative import declarative_base
 
 
 class _TimestampMixin(object):
     """ Timestamping mixin
     """
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = sql.Column(sql.DateTime, default=datetime.utcnow)
     created_at._creation_order = 9998
-    updated_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = sql.Column(sql.DateTime, default=datetime.utcnow)
     updated_at._creation_order = 9998
 
     @staticmethod
@@ -18,7 +18,7 @@ class _TimestampMixin(object):
 
     @classmethod
     def __declare_last__(cls):
-        event.listen(cls, 'before_update', cls._updated_at)
+        sql.event.listen(cls, 'before_update', cls._updated_at)
 
 
 
--- src/backend/api/models/dinosaur.py
+++ src/backend/api/models/dinosaur.py
@@ -1,3 +1,5 @@
+import sqlalchemy as sql
+
 from ..application import db
 from ..mixins.timestamp_mixin import TimestampMixin
 
@@ -5,8 +7,8 @@ from ..mixins.timestamp_mixin import TimestampMixin
 class Dinosaur(db.Model, TimestampMixin):
     __tablename__ = "dinosaur"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String)
+    id = sql.Column(sql.Integer, primary_key=True, autoincrement=True)
+    name = sql.Column(sql.String)
 
     def __repr__(self):
         return "<Dinosaur {}>".format(self.name)
